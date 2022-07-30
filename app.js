fd8fec44e25ae78ff6016f17b896df75766ab5c9
Aryan <aryanbhat324@gmail.com>
2022-07-30 13:31:43 +0530

    Added carousel

--- app.js
+++ app.js
@@ -46,25 +46,80 @@ const navSlide = () =>{
 
   // carousel
 
-  document.querySelectorAll(".carousel").forEach(carousel =>{
-    const items = carousel.querySelectorAll(".carousel-item");
+  document.querySelectorAll(".hcarousel").forEach(hcarousel =>{
+    const items = hcarousel.querySelectorAll(".hcarousel-item");
     const buttonsHtml = Array.from(items , ()=>{
-      return `<span class="carousel-btn"></span>`;
+      return `<span class="hcarousel-btn"></span>`;
     });
-    carousel.insertAdjacentHTML("beforeend",`
-      <div class="carousel-nav">
+    hcarousel.insertAdjacentHTML("beforeend",`
+      <div class="hcarousel-nav">
       ${buttonsHtml.join("")}
     `);
-    const buttons = carousel.querySelectorAll(".carousel-btn");
+    const buttons = hcarousel.querySelectorAll(".hcarousel-btn");
     buttons.forEach((button,i) => {
       button.addEventListener("click", () =>{
-        items.forEach(item => item.classList.remove("carousel-item-selected"));
-        buttons.forEach(button => button.classList.remove("carousel-btn-selected"));
-        items[i].classList.add("carousel-item-selected");
-        button.classList.add("carousel-btn-selected");
+        items.forEach(item => item.classList.remove("hcarousel-item-selected"));
+        buttons.forEach(button => button.classList.remove("hcarousel-btn-selected"));
+        items[i].classList.add("hcarousel-item-selected");
+        button.classList.add("hcarousel-btn-selected");
       });
     });
-    items[0].classList.add("carousel-item-selected");
-    buttons[0].classList.add("carousel-btn-selected");
+    items[0].classList.add("hcarousel-item-selected");
+    buttons[0].classList.add("hcarousel-btn-selected");
   });
 
+ const slides = document.getElementsByClassName("carousel-item");
+const nextButton = document.getElementById("carousel-button-next");
+const prevButton = document.getElementById("carousel-button-prev");
+const dots = document.getElementsByClassName("dot");
+let position = 0;
+const numberOfSlides = slides.length;
+
+function hideAllSlides() {
+    // remove all slides not currently being viewed
+    for (const slide of slides) {
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+    }
+}
+
+
+const handleMoveToNextSlide = function(e) {
+    hideAllSlides();
+  
+    // check if last slide has been reached
+    if (position === numberOfSlides - 1) {
+        position = 0; // go back to first slide
+    } else {
+        // move to next slide
+        position++;
+    }
+    // make current slide visible
+    slides[position].classList.add("carousel-item-visible");
+  
+    // update dot to represent current slide
+    dots[position].classList.add("selected-dot");
+    dots[position].checked = true;
+}
+
+const handleMoveToPrevSlide = function(e) {
+    hideAllSlides();
+    
+    // check if we're on the first slide
+    if (position === 0) {
+        position = numberOfSlides - 1; // move to the last slide
+    } else {
+        // move back one
+        position--;
+    }
+    // make current slide visible
+    slides[position].classList.add("carousel-item-visible");
+  
+    // update dot to represent current slide
+    dots[position].classList.add("selected-dot");
+    dots[position].checked = true;
+}
+
+// listen for slide change events
+nextButton.addEventListener("click", handleMoveToNextSlide);
+prevButton.addEventListener("click", handleMoveToPrevSlide);
